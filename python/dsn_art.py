cb274c2453a76d8919b6abebffa52047c128207f
Adam RasPi <adammgannon@gmail.com>
2023-10-06 20:11:59 -0400

    Basic working.

--- python/dsn_art.py
+++ python/dsn_art.py
@@ -1,68 +1,78 @@
-# Main script. Run parser every few seconds. For each spacecraft 
-# on the board, check its status and update its leds. 
+# Main script. Run parser every few seconds. For each spacecraft
+# on the board, check its status and update its leds.
+import sys
 
-# Git submodule 
+# Git submodule
 from pydsn.parser import *
-# Specific hardware config 
+
+# Specific hardware config
 import hardware
-# Imports for Adafruit Neopixels 
+
+# Imports for Adafruit Neopixels
 import board, neopixel
+
 # Misc utilities
 import time
 
+
 class DsnArt:
+    def __init__(self):
+        # Init the DSN web parser
+        self.parser = DSNParser()
 
-	def __init__(self):
-		# Init the DSN web parser 
-		self.parser = DSNParser()
+        # Get the specific hardware configuration
+        self.hw = hardware.Hardware()
+        self.sc_list = self.hw.get_spacecraft()
 
-		# Get the specific hardware configuration 
-		self.hw = hardware.Hardware()
-		self.sc_list = self.hw.get_spacecraft()
+        # Setup NeoPixels
+        self.pixels = neopixel.NeoPixel(board.D18, 30)
 
-		# Setup NeoPixels 
-		self.pixels = neopixel.NeoPixel(board.D18,30)
+    def update_sc_status(self):
+        data = self.parser.fetch_data()
+        for ant in data.keys():
+            # Get targets (can be multiple)
+            targets = list(data[ant]["targets"].keys())
+            for spacecraft in targets:
+                print(spacecraft)
+                print(spacecraft in self.sc_list)
+                if spacecraft in self.sc_list:
+                    self.set_trx_status(data[ant], spacecraft)
 
-		
-		
-		
+    def set_trx_status(self, ant_dict, sc):
+        # Extract type fields: carrier, data, or none
+        up_type = ant_dict["up_signal"][0]["type"]
+        down_type = ant_dict["down_signal"][0]["type"]
+            
+        # Handle both, otherwise preference downlink color
+        if up_type == down_type:
+            self.hw.set_color(sc, "both",up_type)
+        else:
+            self.hw.set_color(sc, "up", up_type)
+            self.hw.set_color(sc, "down", down_type)
+        
 
-	def update_sc_status(self):
-		data = self.parser.fetch_data()
-		for ant in data.keys():
-			# Get targets (can be multiple)
-			targets = list(data[ant]['targets'].keys())
-			for spacecraft in targets:
-				print(spacecraft)
-				print(spacecraft in self.sc_list)
-				if spacecraft in self.sc_list:
-					self.set_trx_status(data[ant], spacecraft)
+    def set_leds(self):
+        for sc in self.hw.leds:
+            pin_list = self.hw.leds[sc]["pins"]
+            color_tuple = self.hw.leds[sc]["color"]
+            for pin in pin_list:
+                self.pixels[pin] = color_tuple
 
-	def set_trx_status(self,ant_dict,sc):
-		print(ant_dict['up_signal'])
-		up_type = ant_dict['up_signal'][0]['type']
-		self.hw.set_color(sc,'up',up_type)
-		down_type = ant_dict['down_signal'][0]['type']
-		self.hw.set_color(sc,'up',down_type)
-
-
-	def set_leds(self):
-		for sc in self.hw.leds:
-			pin_list = self.hw.leds[sc]['pins']
-			color_tuple = self.hw.leds[sc]['color']
-			for pin in pin_list:
-				self.pixels[pin] = color_tuple
-			
-	def main(self):
-		while True:
-			# Grab new status and update LEDs 
-			self.update_sc_status()
-			self.set_leds()
-			# Wait a few seconds before updating again 
-			time.sleep(10)
+    def main(self):
+        while True:
+            try:
+                # Grab new status and update LEDs
+                self.update_sc_status()
+                self.set_leds()
+                # Wait a few seconds before updating again
+                time.sleep(10)
+            except KeyboardInterrupt:
+                print("Caught interrupt")
+                self.hw.all_off()
+                self.set_leds()
+                sys.exit()
 
 
 if __name__ == "__main__":
-	top = DsnArt()
-	top.main()
-	
+    top = DsnArt()
+    top.main()
--- python/hardware.py
+++ python/hardware.py
@@ -1,85 +1,90 @@
 # Defines the hardware layout of the LEDs, Pi, etc. 
 
 class Hardware:
-	
-	def __init__(self):
-		self.assign_colors()
-		self.assign_leds()
-		print("Hardware Setup Complete.")
+    
+    def __init__(self):
+        self.assign_colors()
+        self.assign_leds()
+        print("Hardware Setup Complete.")
+        
+    def all_off(self):
+        for key in self.leds:
+            self.leds[key]['color'] = (0,0,0)
 
-	def assign_colors(self):
-		self.colors = {}
-		self.colors['up_carrier'] = (123,123,123)
-		self.colors['up_data'] = (0,0,255)
-		self.colors['down_data'] = (0,255,0)
-		self.colors['down_carrier'] = (34,35,36)
+    def assign_colors(self):
+        self.colors = {}
+        self.colors['up_carrier'] = (123,123,123)
+        self.colors['up_data'] = (0,0,255)
+        self.colors['down_data'] = (0,255,0)
+        self.colors['down_carrier'] = (34,35,36)
+        self.colors['both_carrier'] = (50,175,235)
+        self.colors['both_data'] = (104,5,242)
 
-	def assign_leds(self):
-		self.leds = {}
+    def assign_leds(self):
+        self.leds = {}
 
-		# Lunar Reconnaissance Orbiter
-		self.leds['LRO'] = {}
-		self.leds['LRO']['pins'] = [0,1,2]
+        # Lunar Reconnaissance Orbiter
+        self.leds['LRO'] = {}
+        self.leds['LRO']['pins'] = [0,1,2]
 
-		# Lucy
-		self.leds['LUCY'] = {}
-		self.leds['LUCY']['pins'] = [3,4,5]
+        # Lucy
+        self.leds['LUCY'] = {}
+        self.leds['LUCY']['pins'] = [3,4,5]
 
-		# Parker Solar Probe
-		self.leds['SPP'] = {}
-		self.leds['SPP']['pins'] = [6,7,8]
+        # Parker Solar Probe
+        self.leds['SPP'] = {}
+        self.leds['SPP']['pins'] = [6,7,8]
 
-		# Maven
-		self.leds['MVN'] = {}
-		self.leds['MVN']['pins'] = [9,10,11]
+        # Maven
+        self.leds['MVN'] = {}
+        self.leds['MVN']['pins'] = [9,10,11]
 
-		# New Horizons
-		self.leds['NHPC'] = {}
-		self.leds['NHPC']['pins'] = [12,13,14]
+        # New Horizons
+        self.leds['NHPC'] = {}
+        self.leds['NHPC']['pins'] = [12,13,14]
 
-		# OSIRIS-REX
-		self.leds['ORX'] = {}
-		self.leds['ORX']['pins'] = [15,16,17]
+        # OSIRIS-REX
+        self.leds['ORX'] = {}
+        self.leds['ORX']['pins'] = [15,16,17]
 
-		# Mars 2020
-		self.leds['M20'] = {}
-		self.leds['M20']['pins'] = [18,19,20]
+        # Mars 2020
+        self.leds['M20'] = {}
+        self.leds['M20']['pins'] = [18,19,20]
 
-		# Top Row
-		# Juno
-		self.leds['JNO'] = {}
-		self.leds['JNO']['pins'] = [21,22,23]
+        # Top Row
+        # Juno
+        self.leds['JNO'] = {}
+        self.leds['JNO']['pins'] = [21,22,23]
 
-		# MRO
-		self.leds['MRO'] = {}
-		self.leds['MRO']['pins'] = [24,25,26]
+        # MRO
+        self.leds['MRO'] = {}
+        self.leds['MRO']['pins'] = [24,25,26]
 
-		# Psyche
-		self.leds['PSYC'] = {}
-		self.leds['PSYC']['pins'] = [27,28,29]
-
-
+        # Psyche
+        self.leds['PSYC'] = {}
+        self.leds['PSYC']['pins'] = [27,28,29]
 
 
 
-		for key in self.leds:
-			self.leds[key]['color'] = (0,0,0)
-		#self.leds["NHPC"] = {'pins',[9,10,11],'color',(0,0,0)}
-		#self.leds["M20"] = {'pins',[12,13,14],'color',(0,0,0)}
 
-	def get_spacecraft(self):
-		return list(self.leds.keys())
 
-	def set_color(self,sc,direction,carrier):
-		print(carrier)
-		# If no signal or carrier, take no action 
-		if carrier == 'none':
-			print("nah")
-			return 
+        for key in self.leds:
+            self.leds[key]['color'] = (0,0,0)
+        #self.leds["NHPC"] = {'pins',[9,10,11],'color',(0,0,0)}
+        #self.leds["M20"] = {'pins',[12,13,14],'color',(0,0,0)}
 
-		# Otherwise, look up carrier/data color in dictionary 
-		key = direction + "_" + carrier
-		color_tuple = self.colors[key]
+    def get_spacecraft(self):
+        return list(self.leds.keys())
 
-		self.leds[sc]['color'] = color_tuple
-		print(self.leds)
+    def set_color(self,sc,direction,carrier):
+        # If no signal or carrier, take no action 
+        if carrier == 'none':
+            print("nah")
+            return 
+
+        # Otherwise, look up carrier/data color in dictionary 
+        key = direction + "_" + carrier
+        print("getting colors for", key)
+        color_tuple = self.colors[key]
+
+        self.leds[sc]['color'] = color_tuple
